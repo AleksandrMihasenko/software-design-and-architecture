687df55b491ce86c4b11e42b198fa86bcd79a801
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2023-08-20 10:22:56 +0200

    Add Proxy example

---
+++ structural/proxy/description.md
@@ -0,0 +1,8 @@
+# Proxy Pattern
+
+Proxy is a structural design pattern that lets you provide a substitute or placeholder for another object.
+A proxy controls access to the original object, allowing you to perform
+something either before or after the request gets through to the original object.
+
+The proxy object has the same interface as a service,
+which makes it interchangeable with a real object when passed to a client.
---
+++ structural/proxy/example.ts
@@ -0,0 +1,52 @@
+interface Subject {
+  request(): void;
+}
+
+class RealSubject implements Subject {
+  public request(): void {
+    console.log('RealSubject: Handling request.');
+  }
+}
+
+class Proxy implements Subject {
+  private realSubject: RealSubject;
+
+  constructor(realSubject: RealSubject) {
+    this.realSubject = realSubject;
+  }
+
+  public request(): void {
+    if (this.checkAccess()) {
+      this.realSubject.request();
+      this.logAccess();
+    }
+  }
+
+  private checkAccess(): boolean {
+    // Some real checks should go here.
+    console.log('Proxy: Checking access prior to firing a real request.');
+
+    return true;
+  }
+
+  private logAccess(): void {
+    console.log('Proxy: Logging the time of request.');
+  }
+}
+
+/**
+ * The client code is supposed to work with all objects (both subjects and
+ * proxies) via the Subject interface in order to support both real subjects and
+ * proxies. In real life, however, clients mostly work with their real subjects
+ * directly. In this case, to implement the pattern more easily, you can extend
+ * your proxy from the real subject's class.
+ */
+function clientCode(subject: Subject) {
+  subject.request();
+}
+
+const realSubject = new RealSubject();
+clientCode(realSubject);
+
+const proxy = new Proxy(realSubject);
+clientCode(proxy);
